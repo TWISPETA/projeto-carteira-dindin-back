e9040bb0add564207026b7537bc95617c67a366a
Leonardo Melo <lsmelo97@gmail.com>
2022-04-16 09:18:35 -0300

    atualizacao de transacoes

--- conexao.js
+++ conexao.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'dindin',
-    password: 'Restart1!@#',
+    password: '1234',
     port: 5432
 });
 
--- controladores/transacoes.js
+++ controladores/transacoes.js
@@ -4,7 +4,7 @@ const listarTransacoes = async (req, res) => {
     const { usuario } = req;
 
     try {
-        const query = 'select * from transacoes where usuario_id=$1';
+        const query = 'select *, transacoes.descricao as descricao, categorias.descricao as categoria_nome from transacoes join categorias on categorias.id = transacoes.categoria_id where usuario_id=$1';
         const transacao = await conexao.query(query, [usuario.id]);
 
         return res.status(200).json(transacao.rows);
@@ -19,7 +19,7 @@ const detalharTransacao = async (req, res) => {
     const { id } = req.params;
 
     try {
-        const query = 'select * from transacoes where usuario_id=$1 and id=$2';
+        const query = 'select *, transacoes.descricao as descricao, categorias.descricao as categoria_nome from transacoes join categorias on categorias.id = transacoes.categoria_id where usuario_id=$1 and id=$2';
         const { rows: transacao, rowCount } = await conexao.query(query, [usuario.id, id]);
 
         if (rowCount > 0) {
@@ -36,14 +36,14 @@ const detalharTransacao = async (req, res) => {
 const cadastrarTransacao = async (req, res) => {
     const { usuario } = req;
 
-    if (await validarCampos(req)) {
+    if (validarCampos(req)) {
         const { descricao, valor, data, categoria_id, tipo } = req.body;
 
         try {
             const queryInsert = 'insert into transacoes(descricao, valor, data, categoria_id, tipo, usuario_id) values ($1, $2, $3, $4, $5, $6)';
             await conexao.query(queryInsert, [descricao, valor, data, categoria_id, tipo, usuario.id]);
 
-            const querySelect = 'select * from transacoes where usuario_id = $1 order by id desc limit 1';
+            const querySelect = 'select * , transacoes.descricao as descricao, categorias.descricao as categoria_nome from transacoes join categorias on categorias.id = transacoes.categoria_id  where usuario_id = $1 order by id desc limit 1';
             const { rows: ultimaTransacao } = await conexao.query(querySelect, [usuario.id]);
 
             res.status(201).json(ultimaTransacao[0]);
@@ -57,7 +57,7 @@ const cadastrarTransacao = async (req, res) => {
 
 }
 
-const validarCampos = async (req) => {
+const validarCampos = (req) => {
     const { descricao, valor, data, categoria_id, tipo } = req.body;
 
     if (!descricao || !valor || !data || !categoria_id || !tipo) {
@@ -65,40 +65,21 @@ const validarCampos = async (req) => {
     }
 
     if (tipo.toLowerCase() === 'entrada' || tipo.toLowerCase() === 'saida') {
-
-    }
-
-    try {
-        const { rows: categorias } = await conexao.query('select * from categorias');
-
-        const existeId = categorias.find((categoria) => {
-            return categoria.id === categoria_id;
-        });
-
-        if (!existeId) {
-
-            console.log(false)
-            return false;
-        }
-    } catch (e) {
-        return res.status(400).json(e.message);
-    }
-
-    if (tipo.toLowerCase() === 'entrada' || tipo.toLowerCase() === 'saida') {
-        console.log('cheguei');
         return true;
     }
+
+    return false;
 }
 
 const atualizarTransacao = async (req, res) => {
     const { usuario } = req;
     const { id } = req.params;
 
-    if (await validarCampos(req)) {
+    if (validarCampos(req)) {
         const { descricao, valor, data, categoria_id, tipo } = req.body;
 
         try {
-            const querySelect = 'select * from transacoes where id=$1 and usuario_id=$2';
+            const querySelect = 'select * , transacoes.descricao as descricao, categorias.descricao as categoria_nome from transacoes join categorias on categorias.id = transacoes.categoria_id where id=$1 and usuario_id=$2';
             const { rowCount } = await conexao.query(querySelect, [id, usuario.id]);
 
             if (rowCount === 0) {
